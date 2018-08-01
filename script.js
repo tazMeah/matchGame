83bacf91b6b1a4572e938269467f10118c1d13eb
Matthew Kruk <matthew.kruk@gmail.com>
2018-08-01 16:50:40 -0400

    update html, add js: generateCards fn

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 "use strict";
 
-function generateCards() {
-    
+$(document).ready (() => {
+
+generateCards(16);
+
+function generateCards(numOfCards) {
+    numOfCards = numOfCards / 2;
+    console.log(numOfCards);
+    for(let i = 1; i <= numOfCards; i++) {
+        for(let j = 0; j < 2; j++) {
+            $(".cardDeck").append(`<div class="card back" value="${i}">${i}</div>`);
+        }
+    }
 }
+});
