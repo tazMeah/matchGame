fa54fb880a9fc94aef4c5c8be9785e577bc2c9a3
chelsyehannum <chelsye.hannum@gmail.com>
2018-08-01 22:03:07 -0400

    css grid

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 "use strict";
 
-function generateCards() {
-    
-}
+$(document).ready(() => {
+
+    for (let i = 0; i < 16; i++) {
+        $(".grid").append(`<section class='border' id='${i}'></section>`);
+    }
+
+    generateCards(16);
+
+    function generateCards(numOfCards) {
+        numOfCards = numOfCards / 2;
+        console.log(numOfCards);
+        for (let i = 1; i <= numOfCards; i++) {
+            for (let j = 0; j < 2; j++) {
+                $(".cardDeck").append(`<div class="card back" value="${i}">${i}</div>`);
+            }
+        }
+    }
+});
