2202fb0bdaee89d61c79fc353d45c2fec3ab8055
chelsyehannum <chelsye.hannum@gmail.com>
2018-08-02 15:39:05 -0400

    timers

--- script.js
+++ script.js
@@ -36,57 +36,65 @@ $(document).ready(() => {
     fillGrid(memoryDeck.deck);
 
 
-    // --> taz added 8-2-18 10:07am
+    $("#reset").click(function() {
+        location.reload(5000);
+        memoryDeck.randomizeCards()
+    })
+
+
 
     //when you click a card
     $(document).on("click", ".back", function() {
 
-            //add a class to it
-            $(this).toggleClass("back front");
-            console.log($(this).attr("value"));
-            // add background
+        //add a class to it
+        $(this).toggleClass("back front");
+        console.log($(this).attr("value"));
+        // add background
 
-            $(this).css("background", `url("/img/${$(this).attr('value')}.jpg")`);
+        $(this).css("background", `url("/img/${$(this).attr('value')}.jpg")`);
 
-            // if 2 cards have been flipped, i.e. 2 cards have class of "front"
-            if (document.querySelectorAll(".front").length == 2) {
-                // check if the first one's value is equal to the second one's value
-                if ($(".front").eq(0).attr("value") == $(".front").eq(1).attr("value")) {
-                    // if a match
-                    console.log("they match");
-                    // add class "match" to them and remove class "front"
-                    $(".front").toggleClass("front match");
+        // if 2 cards have been flipped, i.e. 2 cards have class of "front"
+        if (document.querySelectorAll(".front").length == 2) {
+            // check if the first one's value is equal to the second one's value
+            if ($(".front").eq(0).attr("value") == $(".front").eq(1).attr("value")) {
+                // if a match
+                console.log("they match");
+                // add class "match" to them and remove class "front"
+                $(".front").toggleClass("front match");
 
-                    // add the score by counting the number of class "match". Put it in the score span
-                    $("span").text($(".match").length);
-
-
+                // add the score by counting the number of class "match". Put it in the score span
+                //$("span").text($(".match").length);
 
 
-                } else {
-                    // if no match
-                    console.log("they don't match");
-                    // remove class front from all
-                    $(".front").removeClass("front")
 
-                }
+
+            } else {
+                // if no match
+                console.log("they don't match");
+                // remove class front from all
+                $(".front").removeClass("front")
 
             }
-
-        })
-        // <-- end taz added 8-2-2018 10:07am 
-        // const timeleft = 15;
-        // const downloadTimer = setInterval(function() {
-        //     timeleft--;
-        //     document.getElementById("countdownTimer").textContent = timeleft;
-        //     if (timeleft <= 0)
-        //         clearInterval(downloadTimer);
-        // }, 1000);
+        }
+    })
 
 
-        //$(".back").css("background-image", `url("${$('.card').attr('value').jpg}")`);
-        
-
+    $("#start").click(function() {
+        let counter = 8;
+        setInterval(function() {
+            counter--;
+            if (counter >= 0) {
+                $("#count").text(counter);
+            }
+            if (counter === 0) {
+                clearInterval(counter);
+            }
+        }, 1000);
+    });
 
 
 });
+
+//$(".back").css("background-image", `url("${$('.card').attr('value').jpg}")`);
+
+//});
