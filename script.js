4a26c9a02349387f8f0fe2211486f0f25cc5a452
Matthew Kruk <matthew.kruk@gmail.com>
2018-08-02 10:00:45 -0400

    refactor js for class: CardDeck

--- script.js
+++ script.js
@@ -2,15 +2,35 @@
 
 $(document).ready (() => {
 
-generateCards(16);
-
-function generateCards(numOfCards) {
-    numOfCards = numOfCards / 2;
-    console.log(numOfCards);
-    for(let i = 1; i <= numOfCards; i++) {
-        for(let j = 0; j < 2; j++) {
-            $(".cardDeck").append(`<div class="card back" value="${i}">${i}</div>`);
+    function fillGrid(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            $(".card_deck").append(arr[i]);
         }
     }
-}
+
+    class CardDeck {
+        constructor(numOfCards) {
+            this.deck = [];
+            this.numOfCards = numOfCards;
+        }
+        generateCards(numOfCards) {
+            numOfCards = numOfCards / 2;
+            const deck = [];
+            for(let i = 1; i <= numOfCards; i++) {
+                for(let j = 0; j < 2; j++) {
+                    this.deck.push(`<div class="card back" value="${i}">${i}</div>`);
+                }
+            }
+        }
+        randomizeCards() {
+            this.deck.sort( function (){
+                return 0.5 - Math.random();
+            });
+        }
+    }
+
+    let memoryDeck = new CardDeck();
+    memoryDeck.generateCards(16);
+    memoryDeck.randomizeCards();
+    fillGrid(memoryDeck.deck); 
 });
