df7bc5c8b971f17af218c9a4dd6c35800e363247
chelsyehannum <chelsye.hannum@gmail.com>
2018-08-02 10:37:32 -0400

    temporary commit 10:37

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 
 $(document).ready(() => {
 
-    for (let i = 0; i < 16; i++) {
-        $(".grid").append(`<section class='border' id='${i}'></section>`);
-    }
 
-    generateCards(16);
 
-    function generateCards(numOfCards) {
-        numOfCards = numOfCards / 2;
-        console.log(numOfCards);
-        for (let i = 1; i <= numOfCards; i++) {
-            for (let j = 0; j < 2; j++) {
-                $(".cardDeck").append(`<div class="card back" value="${i}">${i}</div>`);
-            }
-        }
-    }
+
+
+
+    const timeleft = 15;
+    const downloadTimer = setInterval(function() {
+        timeleft--;
+        document.getElementById("countdownTimer").textContent = timeleft;
+        if (timeleft <= 0)
+            clearInterval(downloadTimer);
+    }, 1000);
+
+
+
 });
